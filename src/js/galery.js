c9358a48382a61fad664ce30027287b573937dea
URG022 <Ulisesriveraguillen519@gmail.com>
2025-09-21 20:08:46 -0600

    Galeria de Imagenes, Apartado de Testimonios y enlace agrega en el Header de la Galeria

---
+++ src/js/galery.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.card-img-top').forEach(img => {
+    img.style.cursor = "pointer";
+    img.addEventListener('click', function () {
+        document.getElementById('modalImg').src = this.src;
+        const modal = new bootstrap.Modal(document.getElementById('imgModal'));
+        modal.show();
+    });
+});
